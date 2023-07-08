3abc13e2e126676a153ed22bb649d0a4d9fda553
Chan Tsz Yeung, Kevin <kevinchan984@gmail.com>
2023-07-08 20:57:35 +0800

    Day 4a done

---
+++ day4/src/main.rs
@@ -0,0 +1,28 @@
+const INPUT: &str = include_str!("../input.txt");
+
+fn parse_input(input: &str) -> (i32, i32, i32, i32) {
+    let splitted: Vec<i32> = input
+        .split(['-', ','])
+        .map(|i| i.parse::<i32>().unwrap())
+        .collect();
+
+    (splitted[0], splitted[1], splitted[2], splitted[3])
+}
+
+fn part_one() {
+    let answer = INPUT
+        .lines()
+        .map(|line| parse_input(line))
+        .filter(|(first_low, first_high, second_low, second_high)| {
+            (first_low >= second_low && first_high <= second_high)
+                || (first_low <= second_low && first_high >= second_high)
+        })
+        .count();
+
+    println!("{answer}");
+}
+
+fn main() {
+    println!("Part 1:");
+    part_one();
+}
